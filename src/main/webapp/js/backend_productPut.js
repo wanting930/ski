38de6897e8a4334ddd1e223c1eb583bee72a76dc
tomo <f22882166@gmail.com>
2023-05-19 22:05:33 +0800

    新增修改web環境測試成功

--- src/main/webapp/js/backend_productPut.js
+++ src/main/webapp/js/backend_productPut.js
@@ -1,16 +1,36 @@
-fetch('http://localhost:8080/ski/getAll')
-  .then(response => response.json())
-  .then(data => {
-    // Process the data retrieved from the server
-    renderProducts(data);
-  })
-  .catch(error => {
-    // Handle any errors that occurred during the request
-    console.error(error);
-  });
+//頁面加載完成後執行
+document.addEventListener('DOMContentLoaded', function() {
+  // 獲取商品數據並渲染表格
+  fetchProducts();
 
+  // 為刪除按鈕綁定事件
+  // 為修改按钮也绑定事件
+  document.getElementById('productTableBody').addEventListener('click', function(event) {
+    if (event.target.classList.contains('btn-delete')) {
+      const productID = event.target.dataset.productId;
+      deleteProduct(productID);
+    } else if (event.target.classList.contains('btn-edit')) {
+      const productID = event.target.dataset.productId;
+      jumpupdatepage(productID); // 跳轉到修改頁面
+    }
+  });
+});
+
+// 獲取商品數據並渲染表格
+function fetchProducts() {
+  fetch('http://localhost:8080/ski/getAll')
+    .then(response => response.json())
+    .then(data => {
+      renderProducts(data);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+// 渲染商品表格
 function renderProducts(products) {
-  const tableBody = document.querySelector('tbody');
+  const tableBody = document.getElementById('productTableBody');
   tableBody.innerHTML = '';
 
   products.forEach(product => {
@@ -18,24 +38,22 @@ function renderProducts(products) {
     row.classList.add('align-middle');
 
     const editButton = document.createElement('button');
-    editButton.classList.add('btn', 'btn-secondary');
+    editButton.classList.add('btn', 'btn-secondary', 'btn-edit');
     editButton.textContent = '修改';
-    editButton.addEventListener('click', () => {
-      jumpupdatepage(product.productID);
-    });
+    editButton.dataset.productId = product.productID;
 
     const deleteButton = document.createElement('button');
-    deleteButton.classList.add('btn', 'btn-secondary');
+    deleteButton.classList.add('btn', 'btn-secondary', 'btn-delete');
     deleteButton.textContent = '刪除';
+    deleteButton.dataset.productId = product.productID;
 
-    // 將圖片資料轉換成 Base64 編碼的字串
     const base64Image = btoa(new Uint8Array(product.productImage).reduce((data, byte) => data + String.fromCharCode(byte), ''));
     const imageSrc = `data:image/png;base64,${base64Image}`;
 
     row.innerHTML = `
       <th scope="row">${product.productID}</th>
       <td class="col-2">
-          <img src="${imageSrc}" class="img-thumbnail">
+        <img src="${imageSrc}" class="img-thumbnail">
       </td>
       <td>${product.productClass}</td>
       <td>${product.productName}</td>
@@ -53,35 +71,23 @@ function renderProducts(products) {
   });
 }
 
+// 跳轉到商品頁面
 function jumpupdatepage(productID) {
   const url = `/ski/product/backend_productUpdate.html?id=${productID}`;
   window.location.href = url;
 }
 
+// 刪除商品
 function deleteProduct(productID) {
-  // Call the backend DELETE API here
-  fetch(`http://localhost:8080/ski/deleteProduct/${productID}`, {
-    method: 'DELETE',
+  fetch(`http://localhost:8080/ski/productDelete?productID=${productID}`, {
+    method: 'GET',
   })
     .then(response => response.json())
     .then(data => {
-      // Process the response from the server
       console.log('Product deleted successfully:', data);
-      // You can update the UI or perform any other actions as needed
-      // For example, you can reload the product list after deletion
-      fetch('http://localhost:8080/ski/getAll')
-        .then(response => response.json())
-        .then(data => {
-          renderProducts(data);
-        })
-        .catch(error => {
-          console.error(error);
-        });
+      fetchProducts(); // 重新獲取商品數據並渲染表格
     })
     .catch(error => {
-      // Handle any errors that occurred during the request
       console.error(error);
     });
 }
-
-
--- src/main/webapp/js/productUpdate.js
+++ src/main/webapp/js/productUpdate.js
@@ -1,146 +1,143 @@
-	// 首先，從 URL 查詢參數中獲取商品ID
-	const urlParams = new URLSearchParams(window.location.search);
-	const productID = urlParams.get('id');
-	
-	// 接著，使用這個商品ID來獲取商品詳情
-	fetch(`http://localhost:8080/ski/productSelectByID?productID=${productID}`, {
-	    method: 'GET',
-	    headers: {
-	        'Content-Type': 'application/json'
-	    }
-	})
-	.then(response => response.json())
-	.then(data => {
-	    // 在這裡，data 就是你的商品資訊
-	    // 你可以將它填入商品修改頁面的表單中
-	    fillForm(data[0]);  // 使用 data[0] 而不是 data
-	    console.log(data[0]) // 顯示第一個商品的資訊
-	})
-	.catch((error) => console.error('錯誤:', error));
-	
-	// 最後，定義一個函數來填充表單
-	function fillForm(product) {
-	    document.getElementById('productName').value = product.productName;
-	    document.getElementById('productClass').value = product.productClass;
-	    document.getElementById('productStatus').value = product.productStatus;
-	    document.getElementById('productDetail').value = product.productDetail;
-	    document.getElementById('productPrice').value = product.productPrice;
-	    document.getElementById('productQuantity').value = product.productQuantity;
-	    document.getElementById('productDate').value = product.productDate;
-	    console.log(product)
-	    // 請注意，文件上傳輸入（如商品圖片）可能需要其他處理方式，並且不能僅僅通過 .value 來設置。
-	}
-	// 為"商品修改"按鈕添加點擊事件監聽器
-	document.querySelector(".btn-info").addEventListener("click", updateProduct);
-	
-	function updateProduct() {
-	    console.log('Update button clicked');
-	    // 從表單獲取新的商品資訊
-	    const updatedProduct = {
-	        productID: productID,
-	        productClass: document.getElementById('productClass').value,
-	        productName: document.getElementById('productName').value,
-	        productStatus: document.getElementById('productStatus').value,
-	        productDetail: document.getElementById('productDetail').value,
-	        productPrice: document.getElementById('productPrice').value,
-	        productQuantity: document.getElementById('productQuantity').value,
-	        productDate: document.getElementById('productDate').value
-	        // 商品圖片需要特別處理，此處省略
-	    };
-	
-	    // 向後端API發送更新請求
-	    fetch(`http://localhost:8080/ski/productUpdate`, {
-	        method: 'POST', // 或是其他適合的HTTP方法，如PUT
-	        headers: {
-	            'Content-Type': 'application/json'
-	        },
-	        body: JSON.stringify(updatedProduct) // 將資料轉為JSON格式
-	    })
-	    .then(response => {
-	        if (!response.ok) {
-	            throw new Error('網路連線錯誤，請稍後再試');
-	        }
-	        return response.json();
-	    })
-	    .then(data => {
-	        // 更新成功，導向商品管理頁面
-	        window.location.href = "/ski/product/backend_productPut.html"; 
-	        
-	    })
-	    .catch((error) => console.error('錯誤:', error));
-	}
-	
-	//幫我另外寫在下面
-	$(document).ready(function() {
-	    function validateInput(input, errorMsg) {
-	      if (input.val().trim() === "") {
-	        errorMsg.show();
-	      } else {
-	        errorMsg.hide();
-	      }
-	    }
-	  
-	    function validateSelect(select, errorMsg) {
-	      if (select.val() === "請選擇商品類別") {
-	        errorMsg.show();
-	      } else {
-	        errorMsg.hide();
-	      }
-	    }
-	  
-	    function validateRadio(radioName, errorMsg) {
-	      if ($(`input[name="${radioName}"]:checked`).length === 0) {
-	        errorMsg.show();
-	      } else {
-	        errorMsg.hide();
-	      }
-	    }
-	  
-	    function validateFile(input, errorMsg) {
-	      if (input.get(0).files.length === 0) {
-	        errorMsg.show();
-	      } else {
-	        errorMsg.hide();
-	      }
-	    }
-	  
-	    $("#inputGroupSelect01").on("change", function() {
-	      validateSelect($(this), $(".text-danger:eq(0)"));
-	    });
-	  
-	    $("input[type='text']").on("input", function() {
-	      validateInput($(this), $(this).parent().parent().next());
-	    });
-	  
-	    $("input[type='radio']").on("change", function() {
-	      validateRadio($(this).attr("name"), $(this).parent().parent().parent().next());
-	    });
-	  
-	    $("#inputGroupFile01").on("change", function() {
-	      validateFile($(this), $(this).parent().parent().next());
-	    });
-	  
-	    $("#floatingTextarea2").on("input", function() {
-	      validateInput($(this), $(this).parent().parent().next());
-	    });
-	  
-	    // 隱藏已經有值的紅字錯誤訊息
-	    $(".text-danger").each(function() {
-	      if ($(this).text() !== "") {
-	        $(this).hide();
-	      }
-	    });
-	  });
-	  
-	  $("#inputGroupFile01").on("change", function() {
-	    const file = this.files[0];
-	    const fileTypes = ["image/jpeg", "image/png", "image/gif", "image/webp"];
-	  
-	    if (file && fileTypes.includes(file.type)) {
-	      $(".text-danger:eq(7)").hide();
-	    } else {
-	      $(".text-danger:eq(7)").text("請上傳有效的圖片文件（jpg, png, gif, webp）");
-	      $(".text-danger:eq(7)").show();
-	      this.value = "";
-	    }
-	  });
+// 首先，從 URL 查詢參數中獲取商品ID
+const urlParams = new URLSearchParams(window.location.search);
+const productID = urlParams.get('id');
+
+// 接著，使用這個商品ID來獲取商品詳情
+fetch(`http://localhost:8080/ski/productSelectByID?productID=${productID}`, {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+.then(response => response.json())
+.then(data => {
+    // 在這裡，data 就是你的商品資訊
+    // 你可以將它填入商品修改頁面的表單中
+    fillForm(data[0]);  // 使用 data[0] 而不是 data
+    console.log(data[0]) // 顯示第一個商品的資訊
+})
+.catch((error) => console.error('錯誤:', error));
+
+// 最後，定義一個函數來填充表單
+function fillForm(product) {
+    document.getElementById('productName').value = product.productName;
+    document.getElementById('productClass').value = product.productClass;
+    document.getElementById('productStatus').value = product.productStatus;
+    document.getElementById('productDetail').value = product.productDetail;
+    document.getElementById('productPrice').value = product.productPrice;
+    document.getElementById('productQuantity').value = product.productQuantity;
+    document.getElementById('productDate').value = product.productDate;
+    console.log(product)
+    // 請注意，文件上傳輸入（如商品圖片）可能需要其他處理方式，並且不能僅僅通過 .value 來設置。
+}
+
+// 為"商品修改"按鈕添加點擊事件監聽器
+document.querySelector(".btn-info").addEventListener("click", updateProduct);
+
+function updateProduct() {
+    console.log('Update button clicked');
+    // 從表單獲取新的商品資訊
+    const updatedProduct = new FormData();
+    updatedProduct.append("productID", productID);
+    updatedProduct.append("productClass", document.getElementById('productClass').value);
+    updatedProduct.append("productName", document.getElementById('productName').value);
+    updatedProduct.append("productStatus", document.getElementById('productStatus').value);
+    updatedProduct.append("productDetail", document.getElementById('productDetail').value);
+    updatedProduct.append("productPrice", document.getElementById('productPrice').value);
+    updatedProduct.append("productQuantity", document.getElementById('productQuantity').value);
+    updatedProduct.append("productDate", document.getElementById('productDate').value);
+    // 將商品圖片也加入 FormData 中
+    const productImage = document.getElementById('productImage').files[0];
+    updatedProduct.append("productImage", productImage);
+
+    // 向後端API發送更新請求
+    fetch(`http://localhost:8080/ski/productUpdate`, {
+        method: 'POST', // 或是其他適合的HTTP方法，如PUT
+        body: updatedProduct // 使用 FormData 物件，不需指定 headers
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('網路連線錯誤，請稍後再試');
+        }
+        return response.json();
+    })
+    .then(data => {
+        // 更新成功，導向商品管理頁面
+        window.location.href = "/ski/product/backend_productPut.html"; 
+    })
+    .catch((error) => console.error('錯誤:', error));
+}
+
+$(document).ready(function() {
+    function validateInput(input, errorMsg) {
+      if (input.val().trim() === "") {
+        errorMsg.show();
+      } else {
+        errorMsg.hide();
+      }
+    }
+  
+    function validateSelect(select, errorMsg) {
+      if (select.val() === "請選擇商品類別") {
+        errorMsg.show();
+      } else {
+        errorMsg.hide();
+      }
+    }
+  
+    function validateRadio(radioName, errorMsg) {
+      if ($(`input[name="${radioName}"]:checked`).length === 0) {
+        errorMsg.show();
+      } else {
+        errorMsg.hide();
+      }
+    }
+  
+    function validateFile(input, errorMsg) {
+      if (input.get(0).files.length === 0) {
+        errorMsg.show();
+      } else {
+        errorMsg.hide();
+      }
+    }
+  
+    $("#inputGroupSelect01").on("change", function() {
+      validateSelect($(this), $(".text-danger:eq(0)"));
+    });
+  
+    $("input[type='text']").on("input", function() {
+      validateInput($(this), $(this).parent().parent().next());
+    });
+  
+    $("input[type='radio']").on("change", function() {
+      validateRadio($(this).attr("name"), $(this).parent().parent().parent().next());
+    });
+  
+    $("#inputGroupFile01").on("change", function() {
+      validateFile($(this), $(this).parent().parent().next());
+    });
+  
+    $("#floatingTextarea2").on("input", function() {
+      validateInput($(this), $(this).parent().parent().next());
+    });
+  
+    // 隱藏已經有值的紅字錯誤訊息
+    $(".text-danger").each(function() {
+      if ($(this).text() !== "") {
+        $(this).hide();
+      }
+    });
+
+    $("#inputGroupFile01").on("change", function() {
+      const file = this.files[0];
+      const fileTypes = ["image/jpeg", "image/png", "image/gif", "image/webp"];
+    
+      if (file && fileTypes.includes(file.type)) {
+        $(".text-danger:eq(7)").hide();
+      } else {
+        $(".text-danger:eq(7)").text("請上傳有效的圖片文件（jpg, png, gif, webp）");
+        $(".text-danger:eq(7)").show();
+        this.value = "";
+      }
+    });
+});
