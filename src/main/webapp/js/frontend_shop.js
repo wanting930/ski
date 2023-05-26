319a2b054324d604581f6f9ca65585275f1fcb6c
tomo <f22882166@gmail.com>
2023-05-26 18:18:30 +0800

    追加彈跳視窗

--- src/main/webapp/js/frontend_shop.js
+++ src/main/webapp/js/frontend_shop.js
@@ -1,17 +1,45 @@
+function createAndAttachModal(productDiv, uniqueModalId) {
+    const uniqueModalLabel = 'staticBackdropLabel' + uniqueModalId.replace('staticBackdrop', '');
+
+    // Create modal HTML
+    const modalHtml = `
+    <div class="modal fade" id="${uniqueModalId}" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="${uniqueModalLabel}" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                </div>
+                <div class="modal-body">
+                    已加入購物車成功
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">關閉</button>
+                </div>
+            </div>
+        </div>
+    </div>  
+    `;
+
+    // Attach modal to product div
+    productDiv.innerHTML += modalHtml;
+}
+
 // Helper function to create product element
 function createProductElement(product) {
-	return fetch(`http://localhost:8080/ski/loadImage?productID=${product.productID}`)
-		.then(response => response.json())
-		.then(imageData => {
-			const productDiv = document.createElement('div');
-			productDiv.classList.add('col-3');
-			productDiv.classList.add('p-2');
+    return fetch(`http://localhost:8080/ski/loadImage?productID=${product.productID}`)
+        .then(response => response.json())
+        .then(imageData => {
+            const productDiv = document.createElement('div');
+            productDiv.classList.add('col-3');
+            productDiv.classList.add('p-2');
 
-			if (product.productStatus === "下架中") {
-				productDiv.classList.add('hidden');
-			}
+            if (product.productStatus === "下架中") {
+                productDiv.classList.add('hidden');
+            }
 
-			const productHtml = `
+            const uniqueModalId = 'staticBackdrop' + product.productID;
+
+
+            const productHtml = `
       	<div class="card text-center">
         <a href="http://localhost:8080/ski/product/frontend_productDetail.html?productID=${product.productID}">
         <img src="data:image/png;base64,${imageData.imageData}" alt="商品圖片" style="height: 100px">
@@ -21,25 +49,22 @@ function createProductElement(product) {
               <div class="productPrice">${product.productPrice}元</div>
               </a>
           </div>
-          <div class="btn btn-info addcar" >加入購物車</div>
+          <div class="card text-center">
+            <!-- ...其他程式碼... -->
+    			 <button class="btn btn-info add-to-cart" data-bs-toggle="modal" data-bs-target="#${uniqueModalId}">加入購物車</button>
           </div>
+	  </div>
       `;
 
-			productDiv.innerHTML = productHtml;
+            productDiv.innerHTML = productHtml;
 
-			// Hide product status
-			const productStatusElement = productDiv.querySelector('.productStatus');
-			if (productStatusElement) {
-				productStatusElement.style.display = 'none';
-			}
+            createAndAttachModal(productDiv, uniqueModalId);
 
-			return productDiv;
-		});
+            return productDiv;
+        });
 }
 
-function addcar(){
-	console.log(123);
-}
+
 
 function displayAllProducts() {
 	fetch('http://localhost:8080/ski/getAll')
