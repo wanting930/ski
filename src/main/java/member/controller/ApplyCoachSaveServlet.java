00a52a69d6afd491a87feed470032fc896ede492
Boy <gogo97930@gmail.com>
2023-06-02 07:34:08 +0800

    新增申請教練資格、教練資格審核

---
+++ src/main/java/member/controller/ApplyCoachSaveServlet.java
@@ -0,0 +1,85 @@
+package member.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+import com.google.gson.Gson;
+
+import member.service.CoachService;
+import member.service.impl.CoachServiceImpl;
+import member.vo.Coach;
+import member.vo.Member;
+
+@WebServlet("/member/applyCoachSave") // http://localhost:8080/ski/member/applyCoachSave
+@MultipartConfig(fileSizeThreshold = 1024 * 1024, maxFileSize = 5 * 1024 * 1024, maxRequestSize = 5 * 5 * 1024 * 1024)
+public class ApplyCoachSaveServlet extends HttpServlet {
+	private static final long serialVersionUID = -5916058450028841844L;
+	private CoachService service;
+	private Gson gson;
+
+	@Override
+	public void init() throws ServletException {
+		service = new CoachServiceImpl();
+		gson = new Gson();
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setHeader("Access-Control-Allow-Origin", "*"); // 允許來自所有網域的請求
+		resp.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE"); // 允許的 HTTP 方法
+		resp.setHeader("Access-Control-Allow-Headers", "Content-Type"); // 允許的請求頭
+		resp.setHeader("Access-Control-Allow-Credentials", "true"); // 是否允許帶有憑證的請求
+
+		// 設置中文編碼
+		req.setCharacterEncoding("UTF-8");
+		resp.setContentType("application/json;charset=UTF-8");
+
+		String userID = req.getParameter("userID");
+		String skill = req.getParameter("skill");
+		String background = req.getParameter("background");
+		String introduce = req.getParameter("introduce");
+		String applyStatus = req.getParameter("applyStatus");
+
+		Coach coach = new Coach();
+		coach.setUserID(Integer.valueOf(userID));
+		coach.setSkill(skill);
+		coach.setBackground(background);
+		coach.setIntroduce(introduce);
+		coach.setApplyStatus(applyStatus);
+
+		Part license = req.getPart("license");
+		
+		if(license == null) {
+			coach.setMessage("請上傳相關證照");
+			coach.setSuccessful(false);
+			String coachJson = gson.toJson(coach);
+			resp.getWriter().write(coachJson);
+			resp.getWriter().flush();
+			return;
+		}
+		
+		if(license != null) {
+			// 把圖片都讀到byte陣列中
+			InputStream in = license.getInputStream();
+			byte[] buf = new byte[in.available()];
+			in.read(buf);
+			coach.setLicense(buf);
+		}
+	
+		coach = service.apply(coach);
+
+		String coachJson = gson.toJson(coach);
+		resp.getWriter().write(coachJson);
+		resp.getWriter().flush();
+
+	}
+
+}
---
+++ src/main/java/member/service/impl/CoachServiceImpl.java
@@ -0,0 +1,77 @@
+package member.service.impl;
+
+import java.util.List;
+
+import member.dao.CoachDao;
+import member.dao.impl.CoachDaoImpl;
+import member.service.CoachService;
+import member.vo.Coach;
+import member.vo.Member;
+
+public class CoachServiceImpl implements CoachService {
+
+	private CoachDao dao; // private CoachDao dao = new CoachDaoImpl();
+
+	public CoachServiceImpl() {
+		dao = new CoachDaoImpl();
+	}
+
+	@Override
+	public Coach coachInfo(Integer userID) {
+		return dao.selectByUserId(userID);
+	}
+
+	@Override
+	public Coach apply(Coach coach) {
+		if (dao.selectByUserId(coach.getUserID()) != null) {
+			coach.setMessage("您已申請過，等待審核中");
+			coach.setSuccessful(false);
+			return coach;
+		}
+		if (coach.getBackground().trim() == null || coach.getBackground().trim().isEmpty()) {
+			coach.setMessage("請輸入個人背景、經歷");
+			coach.setSuccessful(false);
+			return coach;
+		}
+		if (coach.getIntroduce().trim() == null || coach.getIntroduce().trim().isEmpty()) {
+			coach.setMessage("請輸入自我介紹");
+			coach.setSuccessful(false);
+			return coach;
+		}
+		final int resultCount = dao.insert(coach);
+		if (resultCount < 1) {
+			coach.setMessage("申請失敗，請聯絡管理員!");
+			coach.setSuccessful(false);
+			return coach;
+		}
+		coach.setMessage("申請成功，待管理員審核");
+		coach.setSuccessful(true);
+		return coach;
+	}
+
+	@Override
+	public List<Coach> findAll() {
+		return dao.selectAll();
+	}
+	
+	@Override
+	public Coach findOne(Integer userID) {
+		return dao.selectById(userID);
+	}
+	
+	@Override
+	public Coach statusChange(Coach coach) {
+		Boolean isSuccess = dao.updateById(coach);
+
+		if (isSuccess == false) {
+			coach.setMessage("審核失敗，請重新審核");
+			coach.setSuccessful(false);
+			return coach;
+		}
+
+		coach.setMessage("審核成功");
+		coach.setSuccessful(true);
+		return coach;
+	}
+
+}
